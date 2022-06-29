5ddc23162ea85e4151212cfca31161c3a6dc83e2
Александр <105676290+EXCLUZ1V41K@users.noreply.github.com>
2022-06-29 18:31:38 +0300

    Create 1.py

---
+++ Задание 2.1/Средний уровень/1.py
@@ -0,0 +1,11 @@
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+if a % 2 == 0 and b % 2 == 0:
+    print("Числа кратные 2")
+else:
+    print("Не кратные 2")
+if a % 3 == 0 and b % 3 == 0 and c % 3 == 0:
+    print("Числа кратные 3")
+else:
+    print("Не кратные 3")
